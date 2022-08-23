2af95d5db76335a24a9558ea23070cc01e30ea5d
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-08-23 16:15:07 +0900

    Modified templates to display posts from database

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Log
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }, 
+}
