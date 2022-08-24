450fc623ff0c23c4989d59a4ae41c37c4c16ab37
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-08-24 18:15:35 +0900

    add more to your website

--- blog/views.py
+++ blog/views.py
@@ -43,3 +43,21 @@ def post_edit(request, pk):
   else:
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', { 'form': form })
+
+@login_required
+def post_draft_list(request):
+  # 未公開ブロック記事
+  posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+  return render(request, 'blog/post_draft_list.html', { 'posts': posts })
+
+@login_required
+def post_publish(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.publish()
+  return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.delete()
+  return redirect('post_list')
