fa2aca8c8f19e355911a038fc5e8806eff2bea16
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-08-25 16:50:20 +0900

    approved comment

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from turtle import pos
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from . models import Post
+from . models import Comment, Post
 from . forms import CommentForm, PostForm
 
 def post_list(request):
@@ -74,3 +74,15 @@ def add_comment_to_post(request, pk):
   else:
     form = CommentForm()
   return render(request, 'blog/add_comment_to_post.html', { 'form': form })
+
+@login_required
+def comment_approve(request, pk):
+  comment = get_object_or_404(Comment, pk=pk)
+  comment.approve()
+  return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+  comment = get_object_or_404(Comment, pk=pk)
+  comment.delete()
+  return redirect('post_detail', pk=comment.post.pk)
